35b1d7e3c4fbba2934e9d827c77247224287da4e
santiagoelautentico <santiago21062002@gmail.com>
2024-01-22 21:42:13 -0300

    adding style to the login page, not finished

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,32 +1,51 @@
-import "./loginForm.css"
-import { useState } from "react"
-export function LoginForm({setUser}){
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const [error, setError] = useState(false)
+import "./loginForm.css";
+import { useState } from "react";
+export function LoginForm({ setUser }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        if(email === "" || password === "") {
-            setError(true)
-            return
-        }
-        setError(false)
-
-        setUser([email])
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (email === "" || password === "") {
+      setError(true);
+      return;
     }
+    setError(false);
 
-    return(
-        <section>
+    setUser([email]);
+  };
 
-            
-            <h1>Login</h1>
-            <form action="" onSubmit={handleSubmit}>
-                <input type="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} />
-                <input type="password" placeholder="Password"  value={password} onChange={e => setPassword(e.target.value)} />
-                <button type="submit">Login</button>
-            </form>
-            {error && <p>Todos los campos son obligatorios</p>}
-        </section>
-    )
+  return (
+    <section className="login_container">
+      <form action="" onSubmit={handleSubmit} className="form">
+        <article>
+          <div>
+            <h2>Email</h2>
+            <input
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div>
+            <h2>Password</h2>
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button className="buttonLogin" type="submit">
+            <div className="topButtonColor"></div>
+            LOGIN
+            <div className="bottomButtonColor"></div>
+          </button>
+        </article>
+      </form>
+      {error && <p>Todos los campos son obligatorios</p>}
+    </section>
+  );
 }
