d306a34a70eae0c7577a2b44e93a88c1a8aeec2f
santiagoelautentico <santiago21062002@gmail.com>
2024-03-16 18:02:21 -0300

    header finished

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,47 +1,52 @@
 import "./header.css";
-import bgRegular from "../assets/bgRegular.mp4";
-import bgTitanes from "../assets/bgTitanes.mp4";
+import bgRegular from "../assets/regularVideo.mp4";
+import { useState } from "react";
 export function Header() {
+  const [muted, setMuted] = useState(true);
+
+  const handleMuted = () => {
+    setMuted(!muted);
+  };
+
   return (
-    <section className="container">
-      <div className="slider-wrapper">
-        <div className="slider">
-          <div id="slider-1">
-            <video
-              src={bgRegular}
-              className="video"
-              autoPlay
-              loop
-              muted
-            ></video>
-            <div className="infoHeader">
-              <img
-                src="/images/Regular-show-the-movie-logo.png"
-                className="headerLogo"
-                alt=""
-              />
-              <p className="headerText">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                Reprehenderit pariatur a corrupti sint deleniti. Corrupti
-                similique itaque esse? Eos reiciendis molestias, voluptatem
-                laudantium ea odio nihil. Natus sunt iure accusantium.
-              </p>
-              <div className="bottonsHeader">
-                <button className="play">
-                  <div className="topButtonColor"></div>
-                  <img src="/images/play.png" className="logoPlay" alt="" />
-                  PLAY
-                  <div className="bottomButtonColor"></div>
-                </button>
-                <button className="play_more">
-                  <div className="topButtonColor2"></div>
-                  More info
-                  <div className="bottomButtonColor2"></div>
-                </button>
-              </div>
-            </div>
-          </div>
+    <section className="container-header">
+      <div className="slider">
+        <video
+          src={bgRegular}
+          className="video"
+          id="video"
+          autoPlay
+          loop
+          muted={muted ? true : undefined}
+        ></video>
+        <div className="infoHeader">
+          <img
+            src="/images/Regular-show-the-movie-logo.png"
+            className="headerLogo"
+            alt=""
+          />
+          <p className="headerText">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Reprehenderit pariatur a corrupti sint deleniti. Corrupti similique
+            itaque esse? Eos reiciendis molestias
+          </p>
         </div>
+        <div className="bottonsHeader">
+          <button className="play">
+            <div className="topButtonColor"></div>
+            <img src="/images/play.png" className="logoPlay" alt="" />
+            PLAY
+            <div className="bottomButtonColor"></div>
+          </button>
+          <button className="play_more">
+            <div className="topButtonColor2"></div>
+            More info
+            <div className="bottomButtonColor2"></div>
+          </button>
+        </div>
+        <button onClick={handleMuted} className="control">
+          <img src="/images/sound.jpg" alt="" />
+        </button>
       </div>
     </section>
   );
